6d6dcffb08642339a4784788bce3e1f8710d091b
juky-verse <poetic.linguist@gmail.com>
2026-05-20 15:44:36 -0500

    Update main.jsx

--- jukyverse-studios-github-ready/src/main.jsx
+++ jukyverse-studios-github-ready/src/main.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
+const script1 = document.createElement("script");
+script1.async = true;
+script1.src = "https://www.googletagmanager.com/gtag/js?id=G-FVF8T1YNCP";
+document.head.appendChild(script1);
 
+const script2 = document.createElement("script");
+script2.innerHTML = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-FVF8T1YNCP');
+`;
+document.head.appendChild(script2);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
